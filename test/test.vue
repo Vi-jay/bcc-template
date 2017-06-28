e9178bd0c6607f4fdaed01a9d348d73d034a1e93
vijay <765139628@qq.com>
2017-06-28 14:54:11 +0800

    Default

---
+++ test/test.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+    export default {
+        //父组件传入子组件属性
+        props:{
+            age:{
+                'type':Number,
+                'default':0
+            }
+        },
+
+        //数据
+        data () {
+            return {
+            }
+        },
+
+        //函数 计算函数 过滤器函数 钩子
+        methods: {},
+        computed: {},
+        filters:{},
+
+        //组件钩子
+        components:{},
+
+        //生命周期钩子
+        created(){
+            console.log("组件被创建")
+        },
+        destroyed(){
+            console.log("组件被销毁")
+        },
+        mounted(){
+            console.log("组件被挂载")
+        }
+    }
+
+</script>
+
+<style lang="less">
+</style>
