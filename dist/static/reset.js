4ccf16d13c7128c3dbf8d657d5d11e1b466b2096
vijay <765139628@qq.com>
2017-06-28 10:04:25 +0800

    Default

---
+++ dist/static/reset.js
@@ -0,0 +1,20 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if(clientWidth>=640){
+                docEl.style.fontSize = '100px';
+            }else{
+                docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+            }
+            setTimeout(function () {
+                document.body.style.fontSize = '0.16rem';
+            })
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
---
+++ src/view/nice.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="abc">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        },
+        methods: {},
+        created(){
+        },
+        computed: {}
+    }
+</script>
+
+<style lang="less">
+    .abc{
+        background: red;
+        width: 300px;
+        height: 300px;
+    }
+</style>
