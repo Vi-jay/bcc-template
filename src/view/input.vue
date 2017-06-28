b9e161830dfff96e3ff189e74487c787d3d42e83
vijay <765139628@qq.com>
2017-06-28 18:14:34 +0800

    Default

---
+++ src/view/input.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <input type="text"  v-model="aac">
+        <span>{{aac}}</span>
+        <slot></slot>
+        <slot name="h1" ></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            aac:{
+                type:[Number,String],
+                default:0
+            }
+        },
+        data () {
+            return {
+                aa:1
+            }
+        },
+        methods: {
+            emit(){
+                this.$emit("alertMe","ViJay");
+            }
+        },
+        created(){
+        },
+        computed: {}
+    }
+</script>
+
+<style lang="less">
+</style>
