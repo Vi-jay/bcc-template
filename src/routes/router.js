ea309200b45ec147d8ae729c4b5db381428f1762
vijay <765139628@qq.com>
2017-06-28 09:41:33 +0800

    Default

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,9 @@
 var Foo = { template: '<div>foo</div>' };
 var Bar = { template: '<div>bar</div>' };
+const nice = resolve => require(['VIEW/nice.vue'], resolve);
 var routes = [
     { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar }
+    { path: '/bar', component: nice }
 ];
 var RouterConfig = {
     routes: routes
