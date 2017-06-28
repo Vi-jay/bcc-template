c372bbe47df4d8b222dab56b917eada267ca12eb
vijay <765139628@qq.com>
2017-06-28 15:01:23 +0800

    Default

--- test/test.vue
+++ test/test.vue
@@ -6,26 +6,36 @@
 <script>
     export default {
         //父组件传入子组件属性
-        props:{
-            age:{
-                'type':Number,
-                'default':0
+        /***
+           * type:原生构造器        //仅有type值时可以简写为 age:Number 无需设置为对象
+             String
+             Number
+             Boolean
+             Function
+             Object
+             Array
+           * default:默认属性值     //可选
+           * required: true         //可选
+         */
+        props: {
+            age: {
+                'type': Number,
+                'default': 0
             }
         },
 
         //数据
         data () {
-            return {
-            }
+            return {}
         },
 
         //函数 计算函数 过滤器函数 钩子
         methods: {},
         computed: {},
-        filters:{},
+        filters: {},
 
         //组件钩子
-        components:{},
+        components: {},
 
         //生命周期钩子
         created(){
